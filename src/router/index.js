9beeeb3f0456906462c370cf6314d7c5008f384f
毛祥东 <maoxd@yinxing.com>
2023-08-04 15:56:16 +0800

    封装图标选择

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ export const constantRoutes = [
  *
  * hidden: true                     // 当设置 true 的时候该路由不会再侧边栏出现 如401，login等页面，或者如一些编辑页面/edit/1
  * alwaysShow: true                 // 当你一个路由下面的 children 声明的路由大于1个时，自动会变成嵌套的模式--如组件页面
- *                                  // 只有一个时，会将那个子路由当做根路由显示在侧边栏--如引导页面
+ *                                  // 只有一个时，会将那个子路由当做根路由显示在侧边栏
  *                                  // 若你想不管路由下面的 children 声明的个数都显示你的根路由
  *                                  // 你可以设置 alwaysShow: true，这样它就会忽略之前定义的规则，一直显示根路由
  * redirect: noRedirect             // 当设置 noRedirect 的时候该路由在面包屑导航中不可被点击
--- src/views/list/base/index.vue
+++ src/views/list/base/index.vue
@@ -5,15 +5,25 @@
   时间：2023年05月29日 10:10:02
 -->
 <template>
-    <div>
-        <PageTitle title="基础列表" subTitle="标准的列表，包含增删改查等基础操作。"></PageTitle>
+  <div>
+    <PageTitle
+      title="基础列表"
+      subTitle="标准的列表，包含增删改查等基础操作。"
+    ></PageTitle>
+    <div class="dome-box">
+       <IconSelect></IconSelect>
     </div>
-    </template>
-    
-    <script setup>
-    </script> 
-    
-    <style scoped lang="less">
-    
-    </style>
+  </div>
+</template>
+<script setup>
+import IconSelect from "@/components/IconSelect/index.vue";
+</script> 
+<style scoped lang="less">
+.dome-box {
+  padding: 15px;
+  background-color: #ffffff;
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
     
