2f46345b61b1e72862b46c57b895770d90776505
毛祥东 <maoxd@yinxing.com>
2023-09-01 15:01:58 +0800

    :art: 天天vite启图案

---
+++ src/plugin/vitePluginStart.js
@@ -0,0 +1,36 @@
+import colors from "picocolors";
+
+export default function vitePluginVueMonitor() {
+  return {
+    name: "ts-start",
+    apply: "serve",
+    enforce: "pre",
+    configureServer(server) {
+      const print = server.printUrls;
+      server.printUrls = () => {
+        const network = server.resolvedUrls?.network[0];
+        const local = server.resolvedUrls?.local[0];
+        if (!network && !local) {
+          console.log(
+            colors.red(
+              "获取IP地址失败,请检查vite.config.ts文件中server.host配置是否正确!\n"
+            )
+          );
+        } else {
+          console.info(
+            colors.green(
+              " _________  _                         ______                           \n" +
+                "|  _   _  |(_)                      .' ____ \\                          \n" +
+                "|_/ | | \\_|__   .--./) .---.  _ .--.| (___ \\_|  .--.   _ .--.   .--./) \n" +
+                "    | |   [  | / /'`\\;/ /__\\\\[ `/'`\\]_.____`. / .'`\\ \\[ `.-. | / /'`\\; \n" +
+                "   _| |_   | | \\ \\._//| \\__., | |   | \\____) || \\__. | | | | | \\ \\._// \n" +
+                "  |_____| [___].',__`  '.__.'[___]   \\______.' '.__.' [___||__].',__`  \n" +
+                "  Author: Etc.End                             Email:710962805@qq.com             \n"
+            )
+          );
+          print();
+        }
+      };
+    },
+  };
+}
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
+import vitePluginStart from './src/plugin/vitePluginStart.js'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/admin-plus",
   plugins: [
     vue(),
+    vitePluginStart(),
     createSvgIconsPlugin({
       // 指定要缓存的图标文件夹
       iconDirs: [path.resolve(process.cwd(), 'src/icons/svg')],
